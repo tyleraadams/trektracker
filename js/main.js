0fbe0868d8737ed2f5778f2280e1cdab23c62c1e
Toph Tucker <tophtucker@gmail.com>
2014-01-21 22:47:56 -0500

    detect whether the page is embedded in an iframe, and wrap or not accordingly
    the trick now is running things off the cms for desktop and off the
    static thingy site for mobile :-/
    
    #todo: should also accept query string parameters

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+// from http://stackoverflow.com/a/326076/120290
+function inIframe() {
+    try {
+        return window.self !== window.top;
+    } catch(err) {
+        return true;
+    }
+}
 
+$( document ).ready(function() {  
+  if(inIframe()) $("body").addClass("iframed");
+});
