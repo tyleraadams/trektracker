cb85e9851736356be8e093e0975174514c5847aa
Toph Tucker <tophtucker@gmail.com>
2014-01-30 16:18:20 -0500

    new function! bbwNumberFormat(1234567) -> "1.23m"
    suffixes are k, m, b, t.
    0.001234 -> "0.00123"
    1e15 -> "1000t"
    fixes #1

--- js/main.js
+++ js/main.js
@@ -73,3 +73,35 @@ $(document).on("mousemove", function(e) {
   mouseArrow.remove();
   mouseArrow = drawArrow(d3.select("#svg-canvas"), [100,200], [e.pageX,e.pageY], 120, true);
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// NUMBER FORMATTING /////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+function bbwNumberFormat(dolla) {
+  var base = Math.max(1, Math.min(1e12, dolla));
+  var scaler = bbwFormatPrefix(base);
+  return parseFloat(scaler.scale(dolla).toPrecision(3))+scaler.symbol;
+}
+var bbw_formatPrefixes = [ "p", "n", "µ", "m", "", "k", "m", "b", "t" ].map(bbw_formatPrefix);
+function bbwFormatPrefix(value, precision) {
+	var i = 0;
+	if (value) {
+		if (value < 0) value *= -1;
+		if (precision) value = d3.round(value, d3_format_precision(value, precision));
+		i = 1 + Math.floor(1e-12 + Math.log(value) / Math.LN10);
+		i = Math.max(-24, Math.min(24, Math.floor((i <= 0 ? i + 1 : i - 1) / 3) * 3));
+	}
+	return bbw_formatPrefixes[4 + i / 3];
+};
+function bbw_formatPrefix(d, i) {
+	var k = Math.pow(10, Math.abs(4 - i) * 3);
+	return {
+		scale: i > 4 ? function(d) {
+			return d / k;
+		} : function(d) {
+			return d * k;
+		},
+		symbol: d
+	};
+}
