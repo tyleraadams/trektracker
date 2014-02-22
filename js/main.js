6846518ffd623f1bc341a9d8f8f4b85aaa3c25a0
Toph Tucker <tophtucker@gmail.com>
2014-02-22 00:28:50 -0500

    added helper function to get query string

--- js/main.js
+++ js/main.js
@@ -26,6 +26,21 @@ $( document ).ready(function() {
   if(inIframe()) $("body").addClass("iframed");
 });
 
+// store query string in urlParams
+// from http://stackoverflow.com/a/2880929/120290
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
 //////////////////////////////////////////////////////////////////////////////////////////
 // DRAWING FUNCTIONS /////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
